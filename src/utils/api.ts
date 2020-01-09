f185117355c1addd5f0018f1cc2fed92e7fee77c
Lee Seungmin <me@2sm.kr>
2020-01-10 00:11:33 +0900

    api update

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api.hanukoon.com:3001/api/v1",
+  baseURL: "http://119.193.65.179:3001/api/v1",
   headers: { "Access-Control-Allow-Origin": "*" }
 });
 
